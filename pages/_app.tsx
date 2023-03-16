8876dff4b7cdd1b37c314b360f87df3b97036c08
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-03-16 16:35:23 +0600

    creaated _document and added some eslint rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/globals.css';
 
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Top-courses</title>
+				<link rel="stylesheet" href="favicon.ico" />
+			</Head>
+			<Component {...pageProps} />;
+		</>
+	);
 }
